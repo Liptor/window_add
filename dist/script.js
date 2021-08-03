0d2f37633e00ed0a2aca4d339593733bdf282a40
Ivan Ingovatov <ivan.ingov@gmail.com>
2021-08-03 17:30:19 +0300

    fix: modal windows fix

--- dist/script.js
+++ dist/script.js
@@ -14029,31 +14029,42 @@ window.addEventListener('DOMContentLoaded', () => {
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 const modals = () => {
-  function bindModal(trigger, modal, close) {
-    trigger.addEventListener('click', e => {
-      if (e.target) {
-        e.preventDefault();
-      }
+  function bindModal(triggerSelector, modalSelector, closeSelector) {
+    const trigger = document.querySelectorAll(triggerSelector),
+          modal = document.querySelector(modalSelector),
+          close = document.querySelector(closeSelector);
+    trigger.forEach(item => {
+      item.addEventListener('click', e => {
+        if (e.target) {
+          e.preventDefault();
+        }
 
-      modal.style.display = 'block';
-      document.body.style.overflow = 'hidden';
+        modal.style.display = 'block';
+        document.body.style.overflow = 'hidden'; //document.body.classList.add('modal-open')
+      });
     });
     close.addEventListener('click', () => {
       modal.style.display = 'none';
-      document.body.style.overflow = '';
+      document.body.style.overflow = ''; //document.body.classList.remove('modal-open')
     });
     modal.addEventListener('click', e => {
       if (e.target === modal) {
         modal.style.display = 'none';
-        document.body.style.overflow = '';
+        document.body.style.overflow = ''; //document.body.classList.remove('modal-open')
       }
     });
   }
 
-  const callEngineerBtn = document.querySelector('.popup_engineer_btn'),
-        modalEngineer = document.querySelector('.popup_engineer'),
-        modalEngineerClose = document.querySelector('.popup_engineer .popup_close');
-  bindModal(callEngineerBtn, modalEngineer, modalEngineerClose);
+  function showModalByTime(selector, time) {
+    setTimeout(() => {
+      document.querySelector(selector).style.display = 'block';
+      document.body.style.overflow = 'hidden';
+    }, time);
+  }
+
+  bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
+  bindModal('.phone_link', '.popup', '.popup .popup_close');
+  showModalByTime('.popup', 3000);
 };
 
 /* harmony default export */ __webpack_exports__["default"] = (modals);
